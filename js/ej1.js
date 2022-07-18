c7079091454262236307718a85d29bcef9ecf71c
NicoFara <nicolas.fara.romero@gmail.com>
2022-07-18 19:32:41 -0300

    Ejercicio 3 de arrays finalizado y di formato a los archivos

--- js/ej1.js
+++ js/ej1.js
@@ -1,7 +1,20 @@
-const meses =["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
 document.write("<ol>");
-for(let i=0; i<meses.length; i++) {
-    document.write(`<li>${meses[i]}</li>`);
+for (let i = 0; i < meses.length; i++) {
+  document.write(`<li>${meses[i]}</li>`);
 }
-document.write("</ol>")
+document.write("</ol>");
